875a7a6823433fc990200be012db087e85af2481
singularitti <singularitti@outlook.com>
2023-12-26 01:26:40 -0500

    Add `vertices`

--- src/SimpleGraphs.jl
+++ src/SimpleGraphs.jl
@@ -1,11 +1,13 @@
 module SimpleGraphs
 
-export adj
+export adj, vertices
 
 abstract type AbstractSimpleGraph end
 
 adj(g::AbstractSimpleGraph) = g.fadjlist
 adj(g::AbstractSimpleGraph, v) = g.fadjlist[v]
+
+vertices(g::AbstractSimpleGraph) = Base.OneTo(nv(g))
 
 include("simpleedge.jl")
 include("simplegraph.jl")
