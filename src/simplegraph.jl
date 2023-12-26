836ab483152cd50a6cd839362bae2a02de0a5054
singularitti <singularitti@outlook.com>
2023-12-26 00:43:59 -0500

    Add other methods to `SimpleGraph`

--- src/simplegraph.jl
+++ src/simplegraph.jl
@@ -1,4 +1,14 @@
+export nv, add_vertex!
+
 mutable struct SimpleGraph{T<:Integer} <: AbstractSimpleGraph{T}
     ne::Int
     fadjlist::Vector{Vector{T}}
+end
+SimpleGraph() = SimpleGraph(0, [])
+
+nv(g::SimpleGraph) = length(g.fadjlist)
+
+function add_vertex!(g::SimpleGraph{T}) where {T}
+    push!(g.fadjlist, Vector{T}())
+    return g
 end
