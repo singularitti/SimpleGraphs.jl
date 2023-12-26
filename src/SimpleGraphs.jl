82dc171ee17f99ddf2334d81568744ced9346644
singularitti <singularitti@outlook.com>
2023-12-26 00:43:28 -0500

    Add `adj` for `AbstractSimpleGraph`

--- src/SimpleGraphs.jl
+++ src/SimpleGraphs.jl
@@ -1,6 +1,11 @@
 module SimpleGraphs
 
+export adj
+
 abstract type AbstractSimpleGraph{T<:Integer} end
+
+adj(g::AbstractSimpleGraph) = g.fadjlist
+adj(g::AbstractSimpleGraph{T}, v::T) where {T<:Integer} = g.fadjlist[v]
 
 include("simplegraph.jl")
 include("simpledigraph.jl")
